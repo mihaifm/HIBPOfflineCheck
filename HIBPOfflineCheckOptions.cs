eaab5cef911ec608078c3d6669f8a7e123c3228c
David Lindhurst <david@lindhurstdevelopment.com>
2019-02-01 20:12:56 -0500

    - [optimize] moved guard clause to top of GetPasswordStatus method to short circuit before computing hash - [bugfix] fixed the default filename - [bugfix] fixed options defaults - GetString wasn't working properly with empty strings - [cleanup] simplified logic in options dialog - use checkbox state instead of explicit true/false - [cleanup] moved HIBPOfflineColumnProv to its own file. - [cleanup] general cleanup following resharper style recommendations

--- HIBPOfflineCheckOptions.cs
+++ HIBPOfflineCheckOptions.cs
@@ -9,7 +9,7 @@ namespace HIBPOfflineCheck
     public partial class HIBPOfflineCheckOptions : Form
     {
         private HIBPOfflineCheckExt ext;
-        private Options options;
+        private Options _options;
 
         public HIBPOfflineCheckOptions(HIBPOfflineCheckExt ext)
         {
@@ -19,50 +19,43 @@ namespace HIBPOfflineCheck
 
         private void HIBPOfflineCheckOptions_Load(object sender, EventArgs e)
         {
-            this.Icon = AppIcons.Default;
+            Icon = AppIcons.Default;
 
             pb_BannerImage.Image = BannerFactory.CreateBanner(pb_BannerImage.Width, pb_BannerImage.Height, 
                 BannerStyle.Default, Properties.Resources.B48x48_KOrganizer, 
                 "HIBP Offline Check Options", "Manage plugin settings.");
 
-            options = ext.LoadOptions();
+            _options = ext.LoadOptions();
 
-            textBoxFileName.Text = options.HIBPFileName;
-            textBoxColumnName.Text = options.ColumnName;
-            textBoxSecureText.Text = options.SecureText;
-            textBoxInsecureText.Text = options.InsecureText;
-            checkBoxBreachCountDetails.Checked = options.BreachCountDetails;
-            checkBoxWarningDialog.Checked = options.WarningDialog;
-            textBoxWarningDialog.Text = options.WarningDialogText;
+            textBoxFileName.Text = _options.HIBPFileName;
+            textBoxColumnName.Text = _options.ColumnName;
+            textBoxSecureText.Text = _options.SecureText;
+            textBoxInsecureText.Text = _options.InsecureText;
+            checkBoxBreachCountDetails.Checked = _options.BreachCountDetails;
+            checkBoxWarningDialog.Checked = _options.WarningDialog;
+            textBoxWarningDialog.Text = _options.WarningDialogText;
+            textBoxWarningDialog.Enabled = checkBoxWarningDialog.Checked;
 
             textBoxFileName.Select();
             textBoxFileName.Select(0, 0);
 
-            if (checkBoxWarningDialog.Checked == false)
-            {
-                textBoxWarningDialog.Enabled = false;
-            }
-            else
-            {
-                textBoxWarningDialog.Enabled = true;
-            }
         }
 
         private void buttonOK_Click(object sender, EventArgs e)
         {
-            options.HIBPFileName = textBoxFileName.Text;
-            options.ColumnName = textBoxColumnName.Text;
-            options.SecureText = textBoxSecureText.Text;
-            options.InsecureText = textBoxInsecureText.Text;
-            options.BreachCountDetails = checkBoxBreachCountDetails.Checked;
-            options.WarningDialog = checkBoxWarningDialog.Checked;
-            options.WarningDialogText = textBoxWarningDialog.Text;
+            _options.HIBPFileName = textBoxFileName.Text;
+            _options.ColumnName = textBoxColumnName.Text;
+            _options.SecureText = textBoxSecureText.Text;
+            _options.InsecureText = textBoxInsecureText.Text;
+            _options.BreachCountDetails = checkBoxBreachCountDetails.Checked;
+            _options.WarningDialog = checkBoxWarningDialog.Checked;
+            _options.WarningDialogText = textBoxWarningDialog.Text;
 
             var standardFields = PwDefs.GetStandardFields();
 
             foreach (string key in standardFields)
             {
-                if (key == options.ColumnName)
+                if (key == _options.ColumnName)
                 {
                     MessageBox.Show("Column name conflicts with KeePass columns", 
                         " Invalid column name", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -70,13 +63,13 @@ namespace HIBPOfflineCheck
                 }
             }
 
-            ext.SaveOptions(options);
-            this.Close();
+            ext.SaveOptions(_options);
+            Close();
         }
 
         private void buttonCancel_Click(object sender, EventArgs e)
         {
-            this.Close();
+            Close();
         }
 
         private void buttonBrowse_Click(object sender, EventArgs e)
@@ -92,14 +85,7 @@ namespace HIBPOfflineCheck
 
         private void checkBoxWarningDialog_CheckedChanged(object sender, EventArgs e)
         {
-            if (checkBoxWarningDialog.Checked == false)
-            {
-                textBoxWarningDialog.Enabled = false;
-            }
-            else
-            {
-                textBoxWarningDialog.Enabled = true;
-            }
+            textBoxWarningDialog.Enabled = checkBoxWarningDialog.Checked;
         }
     }
 }
---
+++ HIBPOfflineColumnProv.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+using KeePass.Forms;
+using KeePass.Plugins;
+using KeePass.UI;
+using KeePass.Util.Spr;
+using KeePassLib;
+using KeePassLib.Security;
+
+namespace HIBPOfflineCheck
+{
+    public sealed class HIBPOfflineColumnProv : ColumnProvider
+    {
+        private string Status { get; set; }
+        private PwEntry PasswordEntry { get; set; }
+
+        public IPluginHost Host { private get; set; }
+        public Options PluginOptions { private get; set; }
+
+        private bool _insecureWarning;
+        private bool _passwordEdited;
+
+        public HIBPOfflineColumnProv()
+        {
+            HIBPOfflineCheckExt.Host.MainWindow.EntryContextMenu.Opening += ContextMenuStrip_Opening;
+        }
+
+        public override string[] ColumnNames => new [] { PluginOptions.ColumnName };
+
+        public override bool SupportsCellAction(string strColumnName)
+        {
+            return (strColumnName == PluginOptions.ColumnName);
+        }
+
+        private void GetPasswordStatus()
+        {
+            var latestFile = PluginOptions.HIBPFileName;
+            if (!File.Exists(latestFile))
+            {
+                Status = "HIBP file not found";
+                return;
+            }
+
+            string pwdShaStr;
+            using (var sha1 = new SHA1CryptoServiceProvider())
+            {
+                var context = new SprContext(PasswordEntry, Host.Database, SprCompileFlags.All);
+                var password = SprEngine.Compile(PasswordEntry.Strings.GetSafe(PwDefs.PasswordField).ReadString(), context);
+
+                var pwdShaBytes = sha1.ComputeHash(Encoding.UTF8.GetBytes(password));
+                var sb = new StringBuilder(2 * pwdShaBytes.Length);
+
+                foreach (var b in pwdShaBytes)
+                {
+                    sb.AppendFormat("{0:X2}", b);
+                }
+                pwdShaStr = sb.ToString();
+            }
+
+            using (var fs = File.OpenRead(latestFile))
+            using (var sr = new StreamReader(fs))
+            {
+                try
+                {
+                    Status = PluginOptions.SecureText;
+                    var shaLen = pwdShaStr.Length;
+
+                    var low = 0L;
+                    var high = fs.Length;
+
+                    while (low <= high)
+                    {
+                        var middle = (low + high + 1) / 2;
+                        fs.Seek(middle, SeekOrigin.Begin);
+
+                        // Resync with base stream after seek
+                        sr.DiscardBufferedData();
+
+                        var line = sr.ReadLine();
+
+                        if (sr.EndOfStream) break;
+
+                        // We may have read only a partial line so read again to make sure we get a full line
+                        if (middle > 0) line = sr.ReadLine() ?? string.Empty;
+
+                        if (line != null)
+                        {
+                            var compare = string.Compare(pwdShaStr, line.Substring(0, shaLen), StringComparison.Ordinal);
+
+                            if (compare < 0)
+                            {
+                                high = middle - 1;
+                            }
+                            else if (compare > 0)
+                            {
+                                low = middle + 1;
+                            }
+                            else
+                            {
+                                var tokens = line.Split(':');
+                                Status = PluginOptions.InsecureText;
+                                _insecureWarning = true;
+
+                                if (PluginOptions.BreachCountDetails)
+                                {
+                                    Status += " (password count: " + tokens[1].Trim() + ")";
+                                }
+
+                                break;
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    Status = "Failed to read HIBP file";
+                }
+            }
+        }
+
+        public override void PerformCellAction(string strColumnName, PwEntry pe)
+        {
+            if (strColumnName == null || pe == null) { Debug.Assert(false); return; }
+            if (strColumnName != PluginOptions.ColumnName) { return; }
+
+            PasswordEntry = pe;
+
+            GetPasswordStatus();
+            UpdateStatus();
+        }
+
+        private void PwdTouchedHandler(object sender, ObjectTouchedEventArgs e)
+        {
+            PwEntry pe = sender as PwEntry;
+            if (e.Modified)
+            {
+                _passwordEdited = true;
+                PerformCellAction(PluginOptions.ColumnName, pe);
+            }
+        }
+
+        public override string GetCellData(string strColumnName, PwEntry pe)
+        {
+            if (pe == null) return string.Empty;
+
+            //pe.Touched -= PwdTouchedHandler;
+            pe.Touched += PwdTouchedHandler;
+
+            return pe.Strings.GetSafe(PluginOptions.ColumnName).ReadString();
+        }
+
+        private void UpdateStatus()
+        {
+            MainForm mainForm = HIBPOfflineCheckExt.Host.MainWindow;
+            ListView lv = (mainForm.Controls.Find("m_lvEntries", true)[0] as ListView);
+
+            UIScrollInfo scroll = UIUtil.GetScrollInfo(lv, true);
+
+            PasswordEntry.Strings.Set(PluginOptions.ColumnName, new ProtectedString(true, Status));
+
+            mainForm.UpdateUI(false, null, false, null, true, null, true);
+
+            UIUtil.Scroll(lv, scroll, true);
+
+            if (_insecureWarning && _passwordEdited && PluginOptions.WarningDialog)
+            {
+                MessageBox.Show(PluginOptions.WarningDialogText,
+                    "HIBP Offline Check", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            _insecureWarning = false;
+            _passwordEdited = false;
+        }
+
+        private void ContextMenuStrip_Opening(object sender, CancelEventArgs e)
+        {
+            MainForm mainForm = HIBPOfflineCheckExt.Host.MainWindow;
+            ToolStripItem[] items = mainForm.EntryContextMenu.Items.Find("m_ctxEntryMassModify", true);
+
+            if (items.Length > 0)
+            {
+                ToolStripMenuItem ctxEntryMassModify = items[0] as ToolStripMenuItem;
+
+                if (ctxEntryMassModify != null)
+                {
+                    ToolStripItem[] hibpItems = ctxEntryMassModify.DropDownItems.Find("m_ctxEntryHIBP", true);
+
+                    if (hibpItems.Length == 0)
+                    {
+                        ToolStripSeparator separator = new ToolStripSeparator();
+                        ctxEntryMassModify.DropDownItems.Add(separator);
+
+                        ToolStripMenuItem hibpMenuItem = new ToolStripMenuItem()
+                        {
+                            Name = "m_ctxEntryHIBP",
+                            Text = "Have I been pwned?"
+                        };
+
+                        hibpMenuItem.Click += OnMenuHIBP;
+                        ctxEntryMassModify.DropDownItems.Add(hibpMenuItem);
+
+                        ToolStripMenuItem hibpClearMenuItem = new ToolStripMenuItem()
+                        {
+                            Name = "m_ctxEntryHIBPClear",
+                            Text = "Clear pwned status"
+                        };
+
+                        hibpClearMenuItem.Click += OnMenuHIBPClear;
+                        ctxEntryMassModify.DropDownItems.Add(hibpClearMenuItem);
+
+                    }
+                }
+            }
+        }
+
+        private delegate void UpdateStatusDelegate();
+
+        private void OnMenuHIBP(object sender, EventArgs e)
+        {
+            MainForm mainForm = HIBPOfflineCheckExt.Host.MainWindow;
+            PwEntry[] selectedEntries = mainForm.GetSelectedEntries();
+
+            foreach (PwEntry pwEntry in selectedEntries)
+            {
+                PasswordEntry = pwEntry;
+
+                UpdateStatusDelegate updateStatusDel = GetPasswordStatus;
+                IAsyncResult asyncRes = updateStatusDel.BeginInvoke(null, null);
+                updateStatusDel.EndInvoke(asyncRes);
+                
+                UpdateStatus();
+            }
+        }
+
+        private void OnMenuHIBPClear(object sender, EventArgs e)
+        {
+            MainForm mainForm = HIBPOfflineCheckExt.Host.MainWindow;
+            PwEntry[] selectedEntries = mainForm.GetSelectedEntries();
+
+            foreach (PwEntry pwEntry in selectedEntries)
+            {
+                pwEntry.Strings.Remove(PluginOptions.ColumnName);
+            }
+
+            mainForm.UpdateUI(false, null, false, null, true, null, true);
+        }
+    }
+}
